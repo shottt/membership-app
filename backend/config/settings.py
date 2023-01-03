451e3395d2dd4f5d9cf3ebb2bb993062b0fda5a1
shottt <futuregadgetlabomem@gmail.com>
2023-01-03 23:05:12 +0900

    fix: SECRET_KEYを環境変数に退避

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfe^g6%xp39=5h8y1u6ro&*38-2tw37duv^cxd762s+h$qg)+0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
